8226c4ac25196f46fbf7753347b91d8671b5a27c
RomaZinkevich <zra1903@mail.ru>
2025-05-18 13:22:29 +0300

    cors update

--- zephyr/src/main/java/com/zirom/zephyr/config/SecurityConfig.java
+++ zephyr/src/main/java/com/zirom/zephyr/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://taikai-hackathon.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
