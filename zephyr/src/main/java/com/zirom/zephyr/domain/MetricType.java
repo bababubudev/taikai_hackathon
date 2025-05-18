01ebb1a565665d7c12fbfc6edff42553967662c7
RomaZinkevich <zra1903@mail.ru>
2025-05-18 04:48:25 +0300

    added more data to db so had to refactor repository

--- zephyr/src/main/java/com/zirom/zephyr/domain/MetricType.java
+++ zephyr/src/main/java/com/zirom/zephyr/domain/MetricType.java
@@ -2,5 +2,14 @@ package com.zirom.zephyr.domain;
 
 
 public enum MetricType {
-    uv, sp, tm, pm2p5
+    uv,
+    sp,
+    tm,
+    pm2p5,
+    co,
+    no,
+    no2,
+    o3,
+    pm10,
+    so2
 }
